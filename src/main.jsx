fb640284bfc90dc53cbb72cbcd25d7ffc797ca5c
moazabdulhalim <moazabdulhalim@gmail.com>
2023-08-14 14:03:22 +0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -17,8 +17,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Suspense
           fallback={
             // <div className="relative bg-white flex justify-center items-center h-screen w-screen">
-            <Loader />
+            // <Loader />
             // </div>
+            <></>
           }
         >
           {/* <div className="relative h-96">
