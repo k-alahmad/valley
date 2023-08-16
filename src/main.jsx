80c7a6711a0d7f9c974c770bec7a24ec93d21216
Kanaan Alahmad <kanaan92@hotmail.com>
2023-08-16 17:51:59 +0400

    K-Form-data-fixes-v1.4

--- src/main.jsx
+++ src/main.jsx
@@ -10,19 +10,19 @@ import { inject } from "@vercel/analytics";
 import Loader from "./components/UI/Loader/index.jsx";
 inject();
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <Router>
-        <Suspense
-          fallback={
-            <div className="relative bg-white flex justify-center items-center h-screen w-screen">
-              <Loader />
-            </div>
-          }
-        >
-          <App />
-        </Suspense>
-      </Router>
-    </Provider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<Router>
+				<Suspense
+					fallback={
+						<div className='relative bg-white flex justify-center items-center h-screen w-screen'>
+							<Loader />
+						</div>
+					}
+				>
+					<App />
+				</Suspense>
+			</Router>
+		</Provider>
+	</React.StrictMode>,
 );
